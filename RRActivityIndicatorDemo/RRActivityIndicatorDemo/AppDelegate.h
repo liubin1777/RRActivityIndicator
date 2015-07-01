0894f13bc0ad2387248ec2a7b63b4ae9a16ae55d
liubin1777 <binliu1777@gmail.com>
2015-07-01 16:11:30 +0800

    modify some

---
+++ RRActivityIndicatorDemo/RRActivityIndicatorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RRActivityIndicatorDemo
+//
+//  Created by stephenliu on 7/1/15.
+//  Copyright (c) 2015 stephenliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RRActivityIndicatorDemo/RRActivityIndicatorDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RRActivityIndicatorDemo
+//
+//  Created by stephenliu on 7/1/15.
+//  Copyright (c) 2015 stephenliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
