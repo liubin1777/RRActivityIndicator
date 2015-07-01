c6479060c4d2902f6227e8967d4d72119604b384
liubin1777 <binliu1777@gmail.com>
2015-07-01 16:47:05 +0800

    add some code1

---
+++ RRActivityIndicatorDemo/TestIndicator/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  TestIndicator
+//
+//  Created by stephenliu on 7/1/15.
+//  Copyright (c) 2015 stephenliu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "RRActivityIndicator.h"
+#import "PureLayout.h"
+
+@interface ViewController ()
+{
+    RRActivityIndicator *activityIndicator;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    [self clickAdd];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickStart:(id)sender{
+    [activityIndicator startAnimating];
+}
+
+- (IBAction)clickStop:(id)sender{
+    [activityIndicator stopAnimating];
+}
+
+- (void)clickAdd{
+    if (!activityIndicator) {
+        activityIndicator = [[RRActivityIndicator alloc] initWithActivityIndicatorWithImage:[UIImage imageNamed:@"circle"]];
+        [self.view addSubview:activityIndicator];
+        [activityIndicator autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+        [activityIndicator setUserInteractionEnabled:NO];
+    }
+}
+
+@end
